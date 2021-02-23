9d2fc2144d5b2eb002078ed9a67302ff0c254cd0
Hamja Chowdhury <79534502+hamjachow@users.noreply.github.com>
2021-02-23 15:54:50 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+Num1 = input("Enter a number:")
+Symbol = input("Enter a symbol:")
+Num2 = input("Enter another Number:")
+
+if Symbol == "+":
+    print(float(Num1) + float(Num2) )
+elif Symbol == "*":
+    print(float(Num1) * float(Num2) )
+elif Symbol == "-":
+    print(float(Num1) - float(Num2) )
+elif Symbol == "/":
+    print(float(Num1) / float(Num2) )
